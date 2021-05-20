469e9f4c403cbf5df0302597aaa1d85a9d816859
Alan Gervin <alan.gervin@gmail.com>
2021-05-20 15:49:23 -0700

    Now we have a good project final init push.

--- src/main/java/org/perscholas/Alan/test.java
+++ src/main/java/org/perscholas/Alan/test.java
@@ -1,9 +1,10 @@
+package org.perscholas.Alan;
 
 public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("test output");
 	}
 
 }
