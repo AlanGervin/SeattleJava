ea901a8047533ecfcb9ba794238c235e513d1bb0
Alan Gervin <alan.gervin@gmail.com>
2021-05-20 15:31:07 -0700

    another commit

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,10 +1,9 @@
-package org.perscholas.Alan;
 
-public class MainApp {
+public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("running");
+
 	}
 
 }
